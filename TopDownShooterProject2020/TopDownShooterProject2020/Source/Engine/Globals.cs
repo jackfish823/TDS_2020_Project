09e81c5bde842c8da15c907e5ca7613f57631620
Noam <46218256+jackfish823@users.noreply.github.com>
2020-04-05 17:00:34 +0300

    BaseMouse.cs class added
    BaseMouse.cs class affed to source//engine//input, with all of the mouse states and basic functions to show mouse and use the scroll wheel

--- TopDownShooterProject2020/TopDownShooterProject2020/Source/Engine/Globals.cs
+++ TopDownShooterProject2020/TopDownShooterProject2020/Source/Engine/Globals.cs
@@ -18,10 +18,20 @@ namespace TopDownShooterProject2020
 {
     public class Globals
     {
+        public static int screenHeight, screenWidth; // Creatign a global variable for the screen height and width
+            
+
         public static ContentManager content; // Creating global content variable
         public static SpriteBatch spriteBatch; // Creating global spriteBatch variable
 
         public static BaseKeyboard keyboard; // Creating global keyboard variable
 
+
+        public static float GetDistance(Vector2 position, Vector2 target) // Finding distance between 2 positions (vectors)
+        {
+            return (float)Math.Sqrt(Math.Pow(position.X - target.X, 2) + Math.Pow(position.Y - target.Y, 2)); // Basic equation for finding distance between 2 points the distance formula
+        }
+
+       
     }
 }
---
+++ TopDownShooterProject2020/TopDownShooterProject2020/Source/Engine/Input/BaseMouse.cs
@@ -0,0 +1,177 @@
+﻿#region Includes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion
+
+namespace TopDownShooterProject2020
+{
+    public class BaseMouse
+    {
+        public bool dragging, rightDrag;
+
+        public Vector2 newMousePosition, oldMousePosition, firstMousePosition, newMouseAdjustedPos, systemCursorPos, screenLoc; // Vector2's for the mouse positions, newMouseAdjustedPos for zooming, systemCursorPos and screenLoc for bounding the mouse in full screen 
+
+        public MouseState newMouse, oldMouse, firstMouse; // Using 3 mouse states for dragging issues (first mouse get set when you click)
+
+        public BaseMouse() // Constructing mouse and setting basic defaults: setting all mouse states the same starting one
+        {
+            dragging = false; // Dragging by default is false
+
+            newMouse   = Mouse.GetState(); // Getting from monogames framework the mouse state 
+            oldMouse   = newMouse; 
+            firstMouse = newMouse; 
+
+            newMousePosition   = new Vector2(newMouse.Position.X, newMouse.Position.Y); // Setting the mouse position as the new mouse position 
+            oldMousePosition   = new Vector2(newMouse.Position.X, newMouse.Position.Y); 
+            firstMousePosition = new Vector2(newMouse.Position.X, newMouse.Position.Y);
+
+            GetMouseAndAdjust();
+
+        }
+
+        #region Properties
+        public MouseState First
+        {
+            get { return firstMouse; }
+        }
+
+        public MouseState New
+        {
+            get { return newMouse; }
+        }
+
+        public MouseState Old
+        {
+            get { return oldMouse; }
+        }
+        #endregion
+
+        public void Update()
+        {
+            GetMouseAndAdjust();
+
+            if (newMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && oldMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Released)
+            {
+                firstMouse = newMouse;
+                firstMousePosition = newMousePosition = GetScreenPos(firstMouse);
+            }
+        }
+
+        public void UpdateOld()
+        {
+            oldMouse = newMouse;
+            oldMousePosition = GetScreenPos(oldMouse);
+        }
+
+        public virtual float GetDistanceFromClick()
+        {
+            return Globals.GetDistance(newMousePosition, firstMousePosition);
+        }
+        public virtual void GetMouseAndAdjust()
+        {
+            newMouse = Mouse.GetState();
+            newMousePosition = GetScreenPos(newMouse);
+        }
+
+        public int GetMouseWheelChange()
+        {
+            return newMouse.ScrollWheelValue - oldMouse.ScrollWheelValue;
+        }
+
+        public Vector2 GetScreenPos(MouseState mouse)
+        {
+            Vector2 tempVector = new Vector2(mouse.Position.X, mouse.Position.Y);
+
+            return tempVector;
+        }
+
+        public virtual bool LeftClick()
+        {
+            if (newMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && oldMouse.LeftButton != Microsoft.Xna.Framework.Input.ButtonState.Pressed && newMouse.Position.X >= 0 && newMouse.Position.X <= Globals.screenWidth && newMouse.Position.Y <= Globals.screenHeight)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool LeftClickHold()
+        {
+            bool holding = false;
+
+            if (newMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && oldMouse.LeftButton != Microsoft.Xna.Framework.Input.ButtonState.Pressed && newMouse.Position.X >= 0 && newMouse.Position.X <= Globals.screenWidth && newMouse.Position.Y <= Globals.screenHeight)
+            {
+                holding = true;
+                if (Math.Abs(newMouse.Position.X - firstMouse.Position.X) > 8 || Math.Abs(newMouse.Position.Y - firstMouse.Position.Y) >8)
+                {
+                    dragging = true;
+                }
+            }
+
+            return holding;
+        }
+
+
+        public virtual bool LeftClickRelese()
+        {
+            if(newMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Released && oldMouse.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+            {
+                dragging = false;
+                return true;
+            }
+            return false;
+        }
+        public virtual bool RightClick()
+        {
+            if (newMouse.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && oldMouse.RightButton != Microsoft.Xna.Framework.Input.ButtonState.Pressed && newMouse.Position.X >= 0 && newMouse.Position.X <= Globals.screenWidth && newMouse.Position.Y <= Globals.screenHeight)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool RightClickHold()
+        {
+            bool holding = false;
+
+            if (newMouse.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && oldMouse.RightButton != Microsoft.Xna.Framework.Input.ButtonState.Pressed && newMouse.Position.X >= 0 && newMouse.Position.X <= Globals.screenWidth && newMouse.Position.Y <= Globals.screenHeight)
+            {
+                holding = true;
+                if (Math.Abs(newMouse.Position.X - firstMouse.Position.X) > 8 || Math.Abs(newMouse.Position.Y - firstMouse.Position.Y) > 8)
+                {
+                    dragging = true;
+                }
+            }
+
+            return holding;
+        }
+
+
+        public virtual bool RightClickRelese()
+        {
+            if (newMouse.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Released && oldMouse.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+            {
+                dragging = false;
+                return true;
+            }
+            return false;
+        }
+
+        public void SetFirst()
+        {
+
+        }
+    }
+}
