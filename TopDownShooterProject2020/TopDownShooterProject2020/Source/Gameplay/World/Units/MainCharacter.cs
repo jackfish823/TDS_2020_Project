367c6c2fcf8007f30dbbb7e0acb675b6755fec05
Noam <46218256+jackfish823@users.noreply.github.com>
2020-04-06 15:42:31 +0300

    save ignore

--- TopDownShooterProject2020/TopDownShooterProject2020/Source/Gameplay/World/Units/MainCharacter.cs
+++ TopDownShooterProject2020/TopDownShooterProject2020/Source/Gameplay/World/Units/MainCharacter.cs
@@ -50,13 +50,13 @@ namespace TopDownShooterProject2020
                 GameGlobals.CheckScroll(this.position);
             }
 
-            rotation = Globals.RotateToward(position, new Vector2(Globals.mouse.newMousePosition.X, Globals.mouse.newMousePosition.Y) - offset);
-            
+            rotation = Globals.RotateToward(position, new Vector2(Globals.mouse.newMousePosition.X, Globals.mouse.newMousePosition.Y) - offset); // #1 delete if camera (the -offset in the end)
+
             if (Globals.mouse.LeftClick())
             {
                 Vector2 offsetVector = new Vector2(8.59f, -48.43f); // Creating an offset vector so the sprite will come out of the gun 
                 offsetVector = Vector2.Transform(offsetVector, Matrix.CreateRotationZ(rotation)); // rotating the vector so it will be correct to any mouse position
-                GameGlobals.PassProjectile(new GunShot(new Vector2(position.X, position.Y) + offsetVector, this, new Vector2(Globals.mouse.newMousePosition.X, Globals.mouse.newMousePosition.Y) - offset));
+                GameGlobals.PassProjectile(new GunShot(new Vector2(position.X, position.Y) + offsetVector, this, new Vector2(Globals.mouse.newMousePosition.X, Globals.mouse.newMousePosition.Y) - offset)); // #1 delete if camera (the -offset in the end)
             }
 
             base.Update(offset);
